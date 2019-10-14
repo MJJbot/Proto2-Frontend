673863eb709db915226ba3ed246e315725187103
openkmj <42940044+openkmj@users.noreply.github.com>
2019-10-14 23:44:49 +0900

    Add files via upload

---
+++ api/order.js
@@ -0,0 +1,26 @@
+
+export function a1(){
+
+  return [{
+    id: '150',
+    product: 'iPhone6',
+    price: '$699',
+    status: 'processing',
+  },
+  {
+    id: '150',
+    product: 'iPhone6',
+    price: '$699',
+    status: 'processing',
+  }]
+}
+
+export default [
+  {
+    id: '150',
+    product: 'iPhone6',
+    price: '$699',
+    status: 'processing',
+  },
+
+];
