1bf75b3a84e64a45a4f13fe34b214bd9efe16b7c
openkmj <42940044+openkmj@users.noreply.github.com>
2019-10-31 03:46:50 +0900

    Add files via upload

--- store/index.js
+++ store/index.js
@@ -1,5 +1,10 @@
 export const state = () => ({
-  drawer: true
+  drawer: true,
+  messages: [],
+  myself:{},
+  participants: [],
+  chatTitle: '',
+  placeholder: '',
 })
 
 export const mutations = {
@@ -8,5 +13,36 @@ export const mutations = {
   },
   drawer(state, val) {
     state.drawer = val
+  },
+  newMessage: (state, message) => {
+    state.messages = [...state.messages, message];
+  },
+  setParticipants: (state, participants) => {
+    state.participants = participants;
+  },
+  setMyself: (state, myself) => {
+    state.myself = myself;
+  },
+  setMessages: (state, messages) => {
+    state.messages = messages;
+  },
+  setChatTitle: (state, title) => {
+    state.chatTitle = title;
+  },
+  setPlaceholder: (state, placeholder) => {
+    state.placeholder = placeholder;
+  }
+}
+
+export const getters = {
+  getParticipantById: (state) => (id) => {
+    let curr_participant;
+    state.participants.forEach(participant => {
+      if(participant.id == id){
+        curr_participant = participant;
+      }
+    })
+
+    return curr_participant;
   }
 }
